1839134f3f80337d486b5776a4d450f956032ed7
Swastik Dilip <justsyd224@gmail.com>
2023-01-31 22:32:54 +0000

    Added Username

--- rtc-app/src/components/ChatRoom.js
+++ rtc-app/src/components/ChatRoom.js
@@ -5,6 +5,8 @@ import MessageList from "./MessageList";
 export default function ChatRoom() {
   const [messages, setMessages] = useState([]);
   const [messageText, setMessageText] = useState("");
+  const [user, setUser] = useState("");
+  const [showUserField, setShowUserField] = useState(true);
 
   const { sendJsonMessage } = useWebSocket("ws://localhost:8080", {
     onOpen: () => {
@@ -24,12 +26,34 @@ export default function ChatRoom() {
     setMessageText(e.target.value);
   }
 
+  function handleUserNameEntry(e) {
+    setUser(e.target.value);
+  }
+
   return (
     <div>
       <h1>ChatRoom</h1>
-      <MessageList messages={messages} />
-      <input onChange={handleMessageEntry}></input> <br />
-      <button onClick={handleSendMessage}>Send!</button>
+      {showUserField ? (
+        <div>
+          <h3>Please choose your Username!</h3>
+          <input onChange={handleUserNameEntry}></input>
+          <br />
+          <button
+            onClick={() => {
+              setShowUserField(false);
+            }}
+          >
+            Login!
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h3>Welcome! {user}</h3>
+          <MessageList messages={messages} />
+          <input onChange={handleMessageEntry}></input> <br />
+          <button onClick={handleSendMessage}>Send!</button>
+        </div>
+      )}
     </div>
   );
 }
