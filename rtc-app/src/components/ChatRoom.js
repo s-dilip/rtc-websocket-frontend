5f9869621f6f711c729e911c7f71f0a77530045b
Swastik Dilip <justsyd224@gmail.com>
2023-01-31 21:47:43 +0000

    Added sending message

--- rtc-app/src/components/ChatRoom.js
+++ rtc-app/src/components/ChatRoom.js
@@ -4,11 +4,23 @@ import useWebSocket from "react-use-websocket";
 export default function ChatRoom() {
   const [user, setUser] = useState("");
 
-  useWebSocket("ws://localhost:8080", {
-    onOpen: () => {
-      console.log("Websocket Connection Established!");
-    },
-  });
+  const { sendJsonMessage, lastJsonMessage } = useWebSocket(
+    "ws://localhost:8080",
+    {
+      onOpen: () => {
+        console.log("Websocket Connection Established!");
+      },
+    }
+  );
 
-  return <h1>ChatRoom</h1>;
+  function handleSendMessage() {
+    sendJsonMessage({ message: "Hello" });
+  }
+
+  return (
+    <div>
+      <h1>ChatRoom</h1>
+      <button onClick={handleSendMessage}>Send Hello!</button>
+    </div>
+  );
 }
