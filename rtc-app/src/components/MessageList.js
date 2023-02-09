d48a8b58550f5993dacccb27dcc776954191d9d7
Swastik Dilip <justsyd224@gmail.com>
2023-02-09 19:17:18 +0000

    Added robo avatars

--- rtc-app/src/components/MessageList.js
+++ rtc-app/src/components/MessageList.js
@@ -1,11 +1,16 @@
+import "./styles/MessageList.css";
+
 export default function MessageList(props) {
   return (
     <div>
       <h3>Messages</h3>
       {props.messages.map((message, i) => {
+        const roboURL = "https://robohash.org/" + message.sender;
+
         return (
-          <div>
-            <h3 key={i}>{message.sender}</h3>
+          <div key={i}>
+            <img src={roboURL} alt="userImage" className="userRoboImage" />
+            <h3>{message.sender}</h3>
             <p>{message.content}</p>
           </div>
         );
